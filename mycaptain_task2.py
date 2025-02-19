091529792e662e2c9fc8579af2a694d7afec24f7
adityasmestry <adityamestry399@gmail.com>
2025-02-19 12:20:25 +0530

    Add files via upload
    This python code shows how to print all the positive number in the list using for loop.

---
+++ mycaptain_task2.py
@@ -0,0 +1,9 @@
+#Print all positive number from list1 and list2
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+for i in list1:
+    if i>=0:
+        print(i)
+for a in list2:
+    if a>=0:
+        print(a)
